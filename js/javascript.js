eba21bb2893c94ae3984d7a311bc9c8af61dcdab
andrea-del-sarto <37538844+andrea-del-sarto@users.noreply.github.com>
2020-11-20 14:17:04 +0100

    fix

--- js/javascript.js
+++ js/javascript.js
@@ -81,7 +81,7 @@ L.tileLayer(
   "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",
   {
     maxZoom: 9,
-    minZoom: 5,
+    minZoom: 4,
     attribution:
       '<a href="https://www.openstreetmap.org/">OpenStreetMap</a>' +
       '|<a href="https://www.mapbox.com/">Mapbox</a>',
